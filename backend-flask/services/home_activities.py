eda4b6c8d46ec0f6f5f8ea2518f7590b0bca3c4f
R24Amine <rayes.amin@gmail.com>
2023-03-01 16:32:13 +0000

    change home_activities page

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,8 +4,8 @@ class HomeActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brown',
-      'message': 'Cloud is fun!',
+      'handle':  'Amine Rayes',
+      'message': 'Hello Bootcamp!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -15,7 +15,7 @@ class HomeActivities:
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Worf',
-        'message': 'This post has no honor!',
+        'message': 'Cloud is amazing!',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
@@ -25,7 +25,7 @@ class HomeActivities:
     {
       'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
       'handle':  'Worf',
-      'message': 'I am out of prune juice',
+      'message': 'DevOps is my goal',
       'created_at': (now - timedelta(days=7)).isoformat(),
       'expires_at': (now + timedelta(days=9)).isoformat(),
       'likes': 0,
@@ -34,7 +34,7 @@ class HomeActivities:
     {
       'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
       'handle':  'Garek',
-      'message': 'My dear doctor, I am just simple tailor',
+      'message': 'I love navigation',
       'created_at': (now - timedelta(hours=1)).isoformat(),
       'expires_at': (now + timedelta(hours=12)).isoformat(),
       'likes': 0,
