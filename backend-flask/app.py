632e1a0dc724df06df5298d7822411d257215ed7
R24Amine <rayes.amin@gmail.com>
2023-05-10 14:50:36 +0000

    Integrate Rollbar in requirements and in flask app

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -64,6 +64,30 @@ RequestsInstrumentor().instrument()
 #xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 #XRayMiddleware(app, xray_recorder)
 
+# Rollbar----------------------------------------------------------------------
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+# Rollbar initialization------------------------------------------------------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 ######original code
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -80,6 +104,12 @@ cors = CORS(
 #    timestamp = strftime('[%Y-%b-%d %H:%M]')
 #    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #    return response
+
+# a simple endpoint just for testing 
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
