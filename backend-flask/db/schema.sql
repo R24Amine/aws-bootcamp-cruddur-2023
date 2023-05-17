51aa2cac4bec36ecd5a546a67b870f6ebaf7a762
R24Amine <rayes.amin@gmail.com>
2023-05-17 17:49:46 +0000

    Update schema.sql

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -1,29 +1,30 @@
+--We are putting public in front of it because in postgres you can create different namespaces 
+--and make it bey default public becasue when we are builidng microservices we have to make different domains for our app
+--for transition to microservices we can go to seperate databases per domain this is a good practice 
+
+-- https://www.postgresql.org/docs/current/uuid-ossp.html
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- the order matters 
-DROP TABLE IF EXISTS public.users;
+-- forcefully drop our tables if they already exist and the order matters 
+DROP TABLE IF EXISTS public.users cascade;
 DROP TABLE IF EXISTS public.activities;
 -- we will add users tables and activities tables 
-
 CREATE TABLE public.users (
-  uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  display_name text NOT NULL,
-  handle text NOT NULL,
-  email text NOT NULL,
-  cognito_user_id text NOT NULL,
-  created_at TIMESTAMP default current_timestamp NOT NULL
+  uuid UUID default uuid_generate_v4() primary key,
+  display_name text,
+  handle text,
+  cognito_user_id text,
+  created_at timestamp default current_timestamp NOT NULL
 );
 
 CREATE TABLE public.activities (
-  uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  user_uuid UUID NOT NULL,
+  uuid UUID default uuid_generate_v4() primary key,
+  user_uuid UUID REFERENCES public.users(uuid) NOT NULL,
   message text NOT NULL,
-  replies_count integer DEFAULT 0,
-  reposts_count integer DEFAULT 0,
-  likes_count integer DEFAULT 0,
+  replies_count integer default 0,
+  reposts_count integer default 0,
+  likes_count integer default 0,
   reply_to_activity_uuid integer,
-  expires_at TIMESTAMP,
-  created_at TIMESTAMP default current_timestamp NOT NULL
+  expires_at timestamp,
+  created_at timestamp default current_timestamp NOT NULL
 );
-
--- we are putting public in front of it because in postgres you can create different namespaces and make it bey default public becasue when we are builidng microservices we have to make different domains for our app , for transition to microservices we can go to seperate databases per domain this is a good practice 
