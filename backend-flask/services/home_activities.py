9be3a5db4834de3fbd5bf38b2cdcb9ddb46ad6b5
R24Amine <rayes.amin@gmail.com>
2023-05-17 21:31:38 +0000

    Create a db library and modify home activities with real api calls

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 import logging
+from lib.db import pool 
 
 tracer = trace.get_tracer("home.activities")
 
@@ -12,44 +13,32 @@ class HomeActivities:
       now = datetime.now(timezone.utc).astimezone()
 
       span.set_attribute("app.now", now.isoformat())
-      results = [{
-        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Amine Rayes',
-        'message': 'Hello Bootcamp!',
-        'created_at': (now - timedelta(days=2)).isoformat(),
-        'expires_at': (now + timedelta(days=5)).isoformat(),
-        'likes_count': 5,
-        'replies_count': 1,
-        'reposts_count': 0,
-        'replies': [{
-          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'Worf',
-          'message': 'Cloud is amazing!',
-          'likes_count': 0,
-          'replies_count': 0,
-          'reposts_count': 0,
-          'created_at': (now - timedelta(days=2)).isoformat()
-        }],
-      },
-      {
-        'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'handle':  'Worf',
-        'message': 'DevOps is my goal',
-        'created_at': (now - timedelta(days=7)).isoformat(),
-        'expires_at': (now + timedelta(days=9)).isoformat(),
-        'likes': 0,
-        'replies': []
-      },
-      {
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Garek',
-        'message': 'I love navigation',
-        'created_at': (now - timedelta(hours=1)).isoformat(),
-        'expires_at': (now + timedelta(hours=12)).isoformat(),
-        'likes': 0,
-        'replies': []
-      }
-      ]
+      
+      
+      sql = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
+      print(sql)
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          # https://www.psycopg.org/psycopg3/docs/api/cursors.html#psycopg.Cursor.fetchone
+          json = cur.fetchone()
+      return json[0]
       span.set_attribute("app.result_length", len(results))
       return results
